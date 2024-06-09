b1c534faf277f82dd8c74300c3aa8469531cf0e4
Daury P. <dauryjey@gmail.com>
2024-06-09 16:06:17 -0400

    Removed expo

--- rrs-client/app/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import '@tamagui/core/reset.css'
-import { TamaguiProvider, View } from '@tamagui/core' // or 'tamagui'
-import config from './tamagui.config'
-
-export default function App () => (
-  <TamaguiProvider config={config}>
-    <View width={200} height={200} backgroundColor="red" />
-  </TamaguiProvider>
-)
