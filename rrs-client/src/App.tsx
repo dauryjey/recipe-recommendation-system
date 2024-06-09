f470af8f1654da5b7269932535709bd2b54ebb66
Daury P. <dauryjey@gmail.com>
2024-06-09 19:46:33 -0400

    feat: Add basic feature and  dependencies

--- rrs-client/src/App.tsx
+++ rrs-client/src/App.tsx
@@ -1,21 +1,66 @@
-import { Route, Routes } from "react-router-dom";
-
-import IndexPage from "@/pages/index";
-import DocsPage from "@/pages/docs";
-import PricingPage from "@/pages/pricing";
-import BlogPage from "@/pages/blog";
-import AboutPage from "@/pages/about";
+import { useQuery } from "@tanstack/react-query"
+import { useTheme } from "./hooks/use-theme"
+import { useState } from "react"
+import { IMeals } from "./types/meals"
 
 function App() {
+  const { toggleTheme } = useTheme()
+  const [favoritesMeals, setFavoritesMeals] = useState<Meal[]>([])
+
+  const handleAddToFavorite = () => {
+    setFavoritesMeals((prev) => {
+      return [...prev, data![0]]
+    })
+
+    refetch()
+  }
+
+  const { isPending, error, data, isFetching, refetch } = useQuery({
+    queryKey: ["meal"],
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    refetchOnReconnect: false,
+    queryFn: async (): Promise<Meal[]> => {
+      const response = await fetch(
+        "https://www.themealdb.com/api/json/v1/1/random.php"
+      )
+
+      const data = await response.json()
+
+      if (
+        favoritesMeals.find(
+          (meal: Meal) => meal.idMeal === data.meals[0].idMeal
+        )
+      ) {
+        refetch()
+      }
+
+      return data.meals
+    },
+  })
+
   return (
-    <Routes>
-      <Route element={<IndexPage />} path="/" />
-      <Route element={<DocsPage />} path="/docs" />
-      <Route element={<PricingPage />} path="/pricing" />
-      <Route element={<BlogPage />} path="/blog" />
-      <Route element={<AboutPage />} path="/about" />
-    </Routes>
-  );
+    <>
+      <button onClick={toggleTheme}>Change theme</button>
+      <main className="flex flex-col justify-center items-center h-screen">
+        <p>Favorite Meals</p>
+        {favoritesMeals.map((meal) => (
+          <p key={meal.idMeal}>{meal.strMeal}</p>
+        ))}
+        {isPending || isFetching ? (
+          <p>Loading...</p>
+        ) : error ? (
+          <p>Error: {error.message}</p>
+        ) : (
+          <div>
+            <h1>{data[0].strMeal}</h1>
+            <img src={data[0].strMealThumb} alt={data[0].strMeal} />
+            <button onClick={handleAddToFavorite}>Add to favorite</button>
+          </div>
+        )}
+      </main>
+    </>
+  )
 }
 
-export default App;
+export default App
--- rrs-client/src/main.tsx
+++ rrs-client/src/main.tsx
@@ -1,17 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
 
-import App from "./App.tsx";
-import { Provider } from "./provider.tsx";
-import "@/styles/globals.css";
+import App from "./App.tsx"
+import { Provider } from "./provider.tsx"
+import "@/styles/globals.css"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </Provider>
     </BrowserRouter>
-  </React.StrictMode>,
-);
+  </React.StrictMode>
+)
