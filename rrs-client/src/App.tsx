a435f09f3c1c8ba768199a99b2b8a0e5bc78e93c
Daury P. <dauryjey@gmail.com>
2024-06-14 21:13:05 -0400

    chore: Refactor App component, add MealCard component, and update localStorage handling

--- rrs-client/src/App.tsx
+++ rrs-client/src/App.tsx
@@ -1,20 +1,26 @@
 import { useTheme } from "./hooks/use-theme"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { MealCard } from "./components/MealCard"
 import { useFetchMeal } from "./hooks/use-fetchMeal"
-import { Button } from "@nextui-org/button"
 import { ThemeBtn } from "./components/ThemeBtn"
 
 function App() {
   const { theme, toggleTheme } = useTheme()
-  const [favoriteMeals, setFavoritesMeals] = useState<Meal[]>([])
+  const [favoriteMeals, setFavoritesMeals] = useState<Meal[]>(
+    () => JSON.parse(localStorage.getItem("favoriteMeals") || "") as Meal[] | []
+  )
   const { data, error, isPending, isFetching, refetch } = useFetchMeal()
+
+  useEffect(() => {
+    localStorage.setItem("favoriteMeals", JSON.stringify(favoriteMeals))
+  }, [favoriteMeals])
 
   const handleAddToFavorite = () => {
     setFavoritesMeals((prev) => {
-      refetch()
       return [...prev, data as Meal]
     })
+
+    refetch()
   }
 
   if (favoriteMeals.includes(data as Meal)) {
@@ -26,7 +32,15 @@ function App() {
       <header>
         <ThemeBtn theme={theme} toggleTheme={toggleTheme} />
       </header>
-      <main className="flex flex-col justify-center items-center h-[90vh]">
+      <main className="flex flex-col justify-center items-center gap-4">
+        <div className="text-center">
+          <h2 className="text-xl font-bold">Favorite Meals</h2>
+          <ul>
+            {favoriteMeals.map((meal) => (
+              <li key={meal.idMeal}>{meal.strMeal}</li>
+            ))}
+          </ul>
+        </div>
         {isPending || isFetching ? (
           <p>Loading...</p>
         ) : error ? (
@@ -35,6 +49,7 @@ function App() {
           <MealCard
             imgSrc={data.strMealThumb}
             title={data.strMeal}
+            category={data.strCategory}
             description={data.strInstructions}
             videoUrl={data.strYoutube}
             handleAddToFavorite={handleAddToFavorite}
--- rrs-client/src/components/MealCard.tsx
+++ rrs-client/src/components/MealCard.tsx
@@ -12,6 +12,7 @@ import {
 interface MealCardProps {
   imgSrc: string | null
   title: string
+  category: string
   description: string
   videoUrl: string
   handleAddToFavorite: () => void
@@ -20,13 +21,14 @@ interface MealCardProps {
 export const MealCard = ({
   imgSrc,
   title,
+  category,
   description,
   videoUrl,
   handleAddToFavorite,
 }: MealCardProps) => {
   return (
     <Card className="w-[300px]" shadow="md">
-      <CardHeader className="flex-col gap-4">
+      <CardHeader className="flex flex-col gap-4">
         {imgSrc && (
           <Image
             height={200}
@@ -38,10 +40,11 @@ export const MealCard = ({
         )}
         <div className="flex flex-col">
           <p className="text-xl font-bold">{title}</p>
+          <span className="text-gray-400 text-sm">{category}</span>
         </div>
       </CardHeader>
       <CardBody>
-        <p>{description.slice(0, 100).concat("...")}</p>
+        <p className="text-center">{description.slice(0, 100).concat("...")}</p>
       </CardBody>
       <Divider />
       <CardFooter className="flex-row-reverse gap-4">
@@ -49,7 +52,7 @@ export const MealCard = ({
           Watch on Youtube
         </Link>
         <Button
-          variant="ghost"
+          variant="solid"
           color="primary"
           radius="md"
           onClick={handleAddToFavorite}
